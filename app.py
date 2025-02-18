d60d328e03e7e2adbe0f7e1f7ea6e4c676ac74cb
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2025-02-18 11:22:06 -0300

    criação da conexão com o banco de dados

--- app.py
+++ app.py
@@ -1,73 +1,53 @@
 #importar o flask
 from flask import Flask, render_template, request, redirect
 import random
-
+from data import listas_configuracao as listas
 
 app = Flask (__name__)
 
 #aqui vai todas as minhas rotas
 #lista de cores pra alterar o fundo do site
 
-# lista de cores da pagina sobre
-lista_cores1 = ["#ec407",
-               "#f8bbd0",
-               "#f48fb1",
-               "pink",
-               "#e91e63"
-                ]
+
 
 # pagina sobre omar rudberg rs
 @app.route("/sobre")
 
 def pagina_sobre():
 
-    cor_de_fundo = random.choice (lista_cores1)
+    cor_de_fundo = random.choice (listas.lista_cores1)
 
     return render_template("pagina-sobre.html", cor_de_fundo_html = cor_de_fundo)
 
 
-# lista de frases da pagina inicio
-lista_frases = ["Não, não consigo tirar você da minha mente, Yeah yeah.",
-                "Então me atinja no rosto, algo real como a dor.",
-                "E se eu pedir uma gota de chuva, sei que você vai trazer um oceano.",
-                "Lentamente, vejo a luz vermelha, estou preso a esse sonho.",
-                "Toda noite, um novo corpo, fantasia de cada noite, pena que você teve que me perder."
-                ]
 
-# lista de frases da pagina inicio
-lista_imgs = ["omarrudberg_music.jpg",
-              "omar-picture02.avif",
-              "omar-sfilits.jpg",
-              "omar-young.jpg"
-              ]
 
-# lista de cores da pagina inicio
-lista_cores2 = ["red",
-               "#ff5252",
-               "#ef9a9a",
-               "#ffcdd2"
-                ]
 
 # pagina do inicio
 @app.route("/", methods = ["GET"])
 
 def pagina_inicial():
 
-    imagem_aleatoria = random.choice (lista_imgs)
+    imagem_aleatoria = random.choice (listas.lista_imgs)
 
-    frase_aleatoria = random.choice (lista_frases)
+    frase_aleatoria = random.choice (listas.lista_frases)
 
-    cor_de_fundo = random.choice (lista_cores2)
+    cor_de_fundo = random.choice (listas.lista_cores2)
 
     return render_template("pagina-inicial.html", frase_aleatoria_html = frase_aleatoria, imagem_aleatoria_html = imagem_aleatoria, cor_de_fundo_html = cor_de_fundo)
 
 
-# pagina cadastro
+
+
+
+
+
+# PAGINA CADASTRAR FRASE
 @app.route("/cadastro", methods=["GET"])
 
 def pagina_cadastro():
 
-    return render_template("pagina-cadastro.html", frases_html = lista_frases )
+    return render_template("pagina-cadastro.html", frases_html = listas.lista_frases )
 
 
 # pagina cadastrar frase, pra pegar da caixa de texto digitado e aparecer na tela de cadastro
@@ -76,7 +56,7 @@ def post_cadastrarfrase():
 
     frase_vinda_do_html = request.form.get("frase")
 
-    lista_frases.append(frase_vinda_do_html)
+    listas.lista_frases.append(frase_vinda_do_html)
 
     return redirect ("/cadastro")
 
@@ -88,20 +68,24 @@ def delete_frases(indice_frase):
     indice_frase = int(indice_frase)
     
     # exclui a cor da lista atraves do indice
-    lista_frases.pop(indice_frase)
+    listas.lista_frases.pop(indice_frase)
 
 # redireciona para a rota /cadastro cores
     return redirect ("/cadastro")
 
 
-# pagina cadastrar cores
+
+
+
+
+# PAGINA CADASTRAR CORES
 # GET = exibi o nome digitado no link do site
 # GET = enviar uma pagina para o usuario
 @app.route("/cadastrocores", methods=["GET"])
 
 def pagina_cadastro_cores():
 
-    return render_template("pagina-cadastro-cor.html", cores_html = lista_cores2 )
+    return render_template("pagina-cadastro-cor.html", cores_html = listas.lista_cores2 )
 
 # POST = nao exibi o nome digitado no link do site
 # POST = enviar uma informação ou um dado ou uma pagina para o python
@@ -111,7 +95,7 @@ def post_cadastrarcor():
 
     cor_vinda_do_html = request.form.get("cor")
 
-    lista_cores2.append(cor_vinda_do_html) 
+    listas.lista_cores2.append(cor_vinda_do_html) 
 
     return redirect ("/cadastrocores")
 
@@ -124,10 +108,17 @@ def delete_cores(indice_cor):
     indice_cor = int(indice_cor)
     
     # exclui a cor da lista atraves do indice
-    lista_cores2.pop(indice_cor)
+    listas.lista_cores2.pop(indice_cor)
 
 # redireciona para a rota /cadastro cores
     return redirect ("/cadastrocores")
+
+
+
+
+
+
+
 
 #rodar
 app.run(debug=True)
---
+++ data/listas_configuracao.py
@@ -0,0 +1,30 @@
+# lista de cores da pagina sobre
+lista_cores1 = ["#ec407",
+               "#f8bbd0",
+               "#f48fb1",
+               "pink",
+               "#e91e63"
+                ]
+
+
+# lista de frases da pagina inicio
+lista_frases = ["Não, não consigo tirar você da minha mente, Yeah yeah.",
+                "Então me atinja no rosto, algo real como a dor.",
+                "E se eu pedir uma gota de chuva, sei que você vai trazer um oceano.",
+                "Lentamente, vejo a luz vermelha, estou preso a esse sonho.",
+                "Toda noite, um novo corpo, fantasia de cada noite, pena que você teve que me perder."
+                ]
+
+# lista de frases da pagina inicio
+lista_imgs = ["omarrudberg_music.jpg",
+              "omar-picture02.avif",
+              "omar-sfilits.jpg",
+              "omar-young.jpg"
+              ]
+
+# lista de cores da pagina inicio
+lista_cores2 = ["red",
+               "#ff5252",
+               "#ef9a9a",
+               "#ffcdd2"
+                ]
